90467513237672c3c4f9d5c118031c00f38f7812
Renan Medina <renan@silvamedina.com.br>
2024-09-13 18:42:15 -0300

    feat: Add event store package to use event driven code

---
+++ internal/event_store/event_publisher.go
@@ -0,0 +1,65 @@
+package event_store
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/renanmedina/dcp-broadcaster/utils"
+)
+
+type PublishableEvent interface {
+	Name() string
+	ObjectId() string
+	ObjectType() string
+	EventData() map[string]interface{}
+}
+
+type EventHandler interface {
+	Handle(event PublishableEvent)
+}
+
+type EventPublisher struct {
+	handlers map[string][]EventHandler
+}
+
+func NewEventPublisher() *EventPublisher {
+	return &EventPublisher{
+		handlers: make(map[string][]EventHandler),
+	}
+}
+
+func NewEventPublisherWith(handlersSetup map[string][]EventHandler) *EventPublisher {
+	return &EventPublisher{
+		handlers: handlersSetup,
+	}
+}
+
+func (p *EventPublisher) Publish(event PublishableEvent) bool {
+	eventHandlers, exists := p.handlers[event.Name()]
+
+	if exists {
+		go (func(evt PublishableEvent, handlers []EventHandler) {
+			for _, handler := range handlers {
+				logMsg := fmt.Sprintf("Calling handler %s for event %s", reflect.TypeOf(handler), event.Name())
+				utils.GetApplicationLogger().Info(logMsg)
+				handler.Handle(event)
+			}
+		})(event, eventHandlers)
+
+		return true
+	}
+
+	return false
+}
+
+func (p *EventPublisher) Subscribe(eventName string, handler *EventHandler) *EventPublisher {
+	eventHandlers, exists := p.handlers[eventName]
+
+	if !exists {
+		eventHandlers = make([]EventHandler, 0)
+	}
+
+	eventHandlers = append(eventHandlers, *handler)
+	p.handlers[eventName] = eventHandlers
+	return p
+}
---
+++ internal/event_store/events_repository.go
@@ -0,0 +1,42 @@
+package event_store
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/renanmedina/dcp-broadcaster/utils"
+)
+
+const TABLE_NAME = "events"
+
+type EventsRepository struct {
+	db *utils.DatabaseAdapdater
+}
+
+func NewEventsRepository() *EventsRepository {
+	return &EventsRepository{
+		db: utils.GetDatabase(),
+	}
+}
+
+func (r *EventsRepository) Save(event PublishableEvent) error {
+	eventData, err := json.Marshal(event.EventData())
+
+	if err != nil {
+		fmt.Println("Failed marshal event data")
+		panic(err)
+	}
+
+	_, err = r.db.Insert(TABLE_NAME, map[string]interface{}{
+		"event_name":  event.Name(),
+		"object_id":   event.ObjectId(),
+		"object_type": event.ObjectType(),
+		"event_data":  eventData,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
