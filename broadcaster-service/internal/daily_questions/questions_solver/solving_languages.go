d268a5fea14fa220407e2ca45f4b24f50bb4a44d
Renan Medina <renan@silvamedina.com.br>
2024-12-23 18:53:10 -0300

    chore: Ajusting migration flow to new setup

--- broadcaster-service/internal/daily_questions/questions_solver/solving_languages.go
+++ broadcaster-service/internal/daily_questions/questions_solver/solving_languages.go
@@ -1,92 +1,9 @@
 package questions_solver
 
-import "github.com/renanmedina/dcp-broadcaster/utils"
-
 type LanguageInfo struct {
-	LanguageName  string
+	LanguageName  string `gorm:"primaryKey"`
 	FileExtension string
 	Enabled       bool
-}
-
-type SolvingLanguagesRepository struct {
-	db     *utils.DatabaseAdapdater
-	logger *utils.ApplicationLogger
-}
-
-const (
-	QUESTIONS_SOLUTION_LANGUAGES_TABLE_NAME = "daily_questions_solutions"
-	QUESTIONS_SOLUTION_LANGUAGES_FIELDS     = "language_name, file_extension, enabled"
-)
-
-func (r *SolvingLanguagesRepository) GetAll() ([]LanguageInfo, error) {
-	rows, err := r.db.Select(QUESTIONS_SOLUTION_LANGUAGES_FIELDS, QUESTIONS_SOLUTION_LANGUAGES_TABLE_NAME, nil)
-
-	if err != nil {
-		return make([]LanguageInfo, 0), err
-	}
-
-	defer rows.Close()
-
-	var languages []LanguageInfo
-
-	for rows.Next() {
-		var languageInfo LanguageInfo
-		err = rows.Scan(
-			&languageInfo.LanguageName,
-			&languageInfo.FileExtension,
-			&languageInfo.Enabled,
-		)
-
-		if err != nil {
-			panic(err.Error())
-		}
-
-		languages = append(languages, languageInfo)
-	}
-
-	return languages, nil
-}
-
-func (r *SolvingLanguagesRepository) GetAllEnabled() ([]LanguageInfo, error) {
-	rows, err := r.db.Select(
-		QUESTIONS_SOLUTION_LANGUAGES_FIELDS,
-		QUESTIONS_SOLUTION_LANGUAGES_TABLE_NAME,
-		map[string]interface{}{
-			"enabled": 1,
-		},
-	)
-
-	if err != nil {
-		return make([]LanguageInfo, 0), err
-	}
-
-	defer rows.Close()
-
-	var languages []LanguageInfo
-
-	for rows.Next() {
-		var languageInfo LanguageInfo
-		err = rows.Scan(
-			&languageInfo.LanguageName,
-			&languageInfo.FileExtension,
-			&languageInfo.Enabled,
-		)
-
-		if err != nil {
-			panic(err.Error())
-		}
-
-		languages = append(languages, languageInfo)
-	}
-
-	return languages, nil
-}
-
-func NewSolveLanguagesRepository() SolvingLanguagesRepository {
-	return SolvingLanguagesRepository{
-		utils.GetDatabase(),
-		utils.GetApplicationLogger(),
-	}
 }
 
 var SolvingLanguages = map[string]LanguageInfo{
--- broadcaster-service/utils/database.go
+++ broadcaster-service/utils/database.go
@@ -4,22 +4,13 @@ import (
 	"database/sql"
 	"time"
 
-	"github.com/Masterminds/squirrel"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	gormPostgres "gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	gormLogger "gorm.io/gorm/logger"
 )
 
-type DatabaseAdapdater struct {
-	db *sql.DB
-}
-
-type DbRecordable interface {
-	Persisted() bool
-}
-
-var dbAdapter *DatabaseAdapdater
+var dbMigrator *DatabaseMigrator
 var dbConnection *gorm.DB
 
 func init() {
@@ -47,134 +38,17 @@ func initDB() {
 	}
 
 	dbConnection = db
-	dbAdapter = &DatabaseAdapdater{openedDb}
-}
+	dbMigrator, err = NewDatabaseMigrator(openedDb, configs.MIGRATIONS_PATH)
 
-func GetDatabase() *DatabaseAdapdater {
-	return dbAdapter
+	if err != nil {
+		panic(err)
+	}
 }
 
 func GetDatabaseConnection() *gorm.DB {
 	return dbConnection
 }
 
-func (adapter *DatabaseAdapdater) GetConnection() *sql.DB {
-	return adapter.db
-}
-
-func (adapter *DatabaseAdapdater) Insert(tableName string, fieldsAndValues map[string]interface{}) (bool, error) {
-	columns := getKeys(fieldsAndValues)
-	fieldValues := getValuesOrdered(columns, fieldsAndValues)
-
-	_, errInsert := squirrel.
-		Insert(tableName).
-		Columns(columns...).
-		Values(fieldValues...).
-		RunWith(adapter.db).
-		PlaceholderFormat(squirrel.Dollar).
-		Exec()
-
-	if errInsert != nil {
-		return false, errInsert
-	}
-
-	return true, nil
-}
-
-func (adapter *DatabaseAdapdater) Select(fields string, tableName string, wheres interface{}) (*sql.Rows, error) {
-	if fields == "" {
-		fields = "*"
-	}
-
-	rows, errSelect := squirrel.Select(fields).
-		From(tableName).
-		Where(wheres).
-		PlaceholderFormat(squirrel.Dollar).
-		RunWith(adapter.db).
-		Query()
-
-	if errSelect != nil {
-		return nil, errSelect
-	}
-
-	return rows, nil
-}
-
-func (adapter *DatabaseAdapdater) SelectOne(fields string, tableName string, wheres interface{}, orderBys ...string) *squirrel.RowScanner {
-	if fields == "" {
-		fields = "*"
-	}
-
-	scanner := squirrel.Select(fields).
-		From(tableName).
-		Where(wheres).
-		OrderBy(orderBys...).
-		RunWith(adapter.db).
-		PlaceholderFormat(squirrel.Dollar).
-		QueryRow()
-
-	return &scanner
-}
-
-func (adapter *DatabaseAdapdater) Update(tableName string, fieldsAndValues map[string]interface{}, wheres interface{}) (bool, error) {
-	updateBuilder := squirrel.Update(tableName)
-
-	for fieldName, fieldVal := range fieldsAndValues {
-		updateBuilder = updateBuilder.Set(fieldName, fieldVal)
-	}
-
-	updateBuilder.Where(wheres)
-
-	_, errUpdate := updateBuilder.RunWith(adapter.db).PlaceholderFormat(squirrel.Dollar).Exec()
-
-	if errUpdate != nil {
-		return false, errUpdate
-	}
-
-	return true, nil
-}
-
-func (adapter *DatabaseAdapdater) UpdateById(tableName string, id string, fieldsAndValues map[string]interface{}) (bool, error) {
-	wheres := map[string]interface{}{
-		"id": id,
-	}
-
-	return adapter.Update(tableName, fieldsAndValues, wheres)
-}
-
-func (adapter *DatabaseAdapdater) Delete(tableName string, wheres interface{}) (bool, error) {
-	deleteBuilder := squirrel.Delete(tableName)
-	deleteBuilder.Where(wheres)
-
-	_, errDelete := deleteBuilder.RunWith(adapter.db).PlaceholderFormat(squirrel.Dollar).Exec()
-
-	if errDelete != nil {
-		return false, errDelete
-	}
-
-	return true, nil
-}
-
-func getKeys(mapVar map[string]interface{}) []string {
-	keys := make([]string, len(mapVar))
-
-	i := 0
-	for key := range mapVar {
-		keys[i] = key
-		i++
-	}
-
-	return keys
-}
-
-func getValuesOrdered(columns []string, mapVar map[string]interface{}) []interface{} {
-	vals := make([]interface{}, len(mapVar))
-
-	i := 0
-	for _, column := range columns {
-		vals[i] = mapVar[column]
-		i++
-	}
-
-	return vals
+func GetDatabaseMigrator() *DatabaseMigrator {
+	return dbMigrator
 }
